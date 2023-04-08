bc36eb7e0e40acc8fae39bb14f7429b860a30450
nina <ninagbs@abv.bg>
2023-04-08 11:32:15 +0300

    changes about better UX

--- client/src/components/EditStory/EditStory.js
+++ client/src/components/EditStory/EditStory.js
@@ -1,50 +1,58 @@
 import { useEffect, useState } from "react";
 import { useParams, useNavigate } from 'react-router-dom';
+import './Animation.css'
 
 import * as storyService from '../../services/storyService';
 import { useStoryContext } from "../../contexts/StoryContext";
 
 export const EditStory = () => {
-    const [currentStory, setCurrentStory] = useState({});
-    const{storyEdit }= useStoryContext()
-    const { storyId } = useParams();
-    const navigate = useNavigate();
+  const [currentStory, setCurrentStory] = useState({});
+  const { storyEdit } = useStoryContext()
+  const { storyId } = useParams();
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        storyService.getOne(storyId)
-            .then(storyData => {
-                setCurrentStory(storyData);
-            })
-    }, [storyId])
+  useEffect(() => {
+    storyService.getOne(storyId)
+      .then(storyData => {
+        setCurrentStory(storyData);
+      })
+  }, [storyId])
 
-    const onSubmit = (e) => {
-        e.preventDefault();
+  const onSubmit = (e) => {
+    e.preventDefault();
 
-        const storyData = Object.fromEntries(new FormData(e.target));
-        if(Object.values(storyData).some(x=>x==='')){
-          return alert("Моля, попълнете всички полета!")
-      }
-      storyService.edit(storyId, storyData)
-            .then(result => {
-                storyEdit(storyId, result);
-              navigate(`/catalog/${storyId}`)
-            });
-    };
+    const storyData = Object.fromEntries(new FormData(e.target));
+    if (Object.values(storyData).some(x => x === '')) {
+      return alert("Моля, попълнете всички полета!")
+    }
+    storyService.edit(storyId, storyData)
+      .then(result => {
+        storyEdit(storyId, result);
+        navigate(`/catalog/${storyId}`)
+      });
+  };
 
 
-    return(
-        <section id="edit">
-        <div className="form">
-          <h2>Редактирай твоя кратък разказ</h2>
-          <form className="edit-form" method="post" onSubmit = {onSubmit}>
-            
-            <input type="text" name="title" id="album-album" placeholder="Заглавие" defaultValue={currentStory.title} />
-            <input type="text" name="img" id="album-img" placeholder="Снимка https://..." defaultValue={currentStory.img} />
-            <textarea name="description" cols="100" rows="40" placeholder="Твоя кратък разказ" defaultValue={currentStory.description} ></textarea>
+  return (
+    <section id="edit">
 
-            <button type="submit">Редактирай</button>
-          </form>
-        </div>
-      </section>
-    )
+      <br />
+      <div className="typewriter">
+        <div className="slide"><i></i></div>
+        <div className="paper"></div>
+        <div className="keyboard"></div>
+      </div>
+      <div className="form">
+        <h2>Редактирай твоя кратък разказ</h2>
+        <form className="edit-form" method="post" onSubmit={onSubmit}>
+
+          <input type="text" name="title" id="album-album" placeholder="Заглавие" defaultValue={currentStory.title} />
+          <input type="text" name="img" id="album-img" placeholder="Снимка https://..." defaultValue={currentStory.img} />
+          <textarea name="description" cols="100" rows="40" placeholder="Твоя кратък разказ" defaultValue={currentStory.description} ></textarea>
+
+          <button type="submit">Редактирай</button>
+        </form>
+      </div>
+    </section>
+  )
 }
